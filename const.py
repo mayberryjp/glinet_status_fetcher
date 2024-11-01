8a37aca99dbbbaa93b3d996f66771cf147bd4459
JR Mayberry <rimayber@microsoft.com>
2024-11-01 11:44:26 +0900

    fix compose names

--- const.py
+++ const.py
@@ -3,7 +3,7 @@ VERSION="v1.0.1"
 SLEEP_INTERVAL=60
 
 ENTITIES =  {
-    "Temperature": {"type": "temperature", "unit": "C"},
+    "Temperature": {"type": "temperature", "unit": "°C"},
    # "charge_cnt": {"type": "None", "unit": ""},
     "Percent": {"type": "battery", "unit": "%"},
   #  "charging": {"type": "None", "unit": ""},
